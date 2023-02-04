dd0e3bd14e34acd9d26339877592198e3bf49c0f
Eslam Ashraf <71986226+EslamAsHhraf@users.noreply.github.com>
2023-02-04 18:43:49 +0200

    add checkout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,19 @@ import React from 'react'
 import Routers from '../../routes/Routers';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
-const Layout = () => {
+import Carts from '../UI/cart/Carts';
+
+import { useSelector } from 'react-redux';
+
+const Layout = () =>
+{
+  const showCart=useSelector(state=>state.cartUi.cartIsVisible)
     return (
     <div>
-      <Header/>
+        <Header />
+        {showCart && 
+          <Carts />
+        }
        <div>
             <Routers/>
         </div>
--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from "./shopping-cart/cartSlice";
+import cartUiSlice from "./shopping-cart/cartUiSlice";
+
 
 const store = configureStore( {
     reducer: {
-        cart:cartSlice.reducer
+        cart: cartSlice.reducer,
+        cartUi: cartUiSlice.reducer,
     }
 } )
 
